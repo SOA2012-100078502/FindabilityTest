1878aaa8dba20e35efcd9b42a3ef5087f7fdddd1
SOA2012-100078502 <warwar0077@gmail.com>
2016-12-11 17:50:39 +0800

    flight proposal WIP

--- ReplyResolver.R
+++ ReplyResolver.R
@@ -55,9 +55,61 @@ RCpart4<-rbind(RCpart4,RCpart3)
 
 colnames(RCpart4)<-c("RCitem","FlightComb","VCarrier","TotalFare")
 
+#
+### below part is still working on it
+#
 
-###working on it
+#
+#RS1 flight proposal
+#
+RS1FP<-xmlApply(getNodeSet(query1,
+                           "//Fare_MasterPricerTravelBoardSearchReply/
+                           flightIndex[requestedSegmentRef=1]/
+                           groupOfFlights/
+                           propFlightGrDetail/flightProposal/ref"),xmlValue,recursive=TRUE)
+RS1FP<-data.frame(matrix(unlist(RS1FP),ncol=3,byrow=TRUE))
+colnames(RS1FP)<-c("FP1_item","FP1_EFT","FP2_MCX")
+RS1FP$FP1_item<-paste("S",RS1FP$FP1_item,sep = "")
 
+#
+#RS2 flight proposal
+#
+
+RS2FP<-xmlApply(getNodeSet(query1,
+                           "//Fare_MasterPricerTravelBoardSearchReply/
+                           flightIndex[requestedSegmentRef=2]/
+                           groupOfFlights/
+                           propFlightGrDetail/flightProposal/ref"),xmlValue,recursive=TRUE)
+RS2FP<-data.frame(matrix(unlist(RS2FP),ncol=3,byrow=TRUE))
+colnames(RS2FP)<-c("FP2_item","FP2_EFT","FP2_MCX")
+RS2FP$FP2_item<-paste("S",RS2FP$FP2_item,sep = "")
+
+#
+#RS1+RS2 flight proposal
+#
+FPcomb<-expand.grid(RS1FP$FP1_item,RS2FP$FP2_item)
+FPcomb<-data.frame(paste(FPcomb$Var1,FPcomb$Var2,sep=""))
+colnames(FPcomb)<-c("2RScomb")
+
+FPcomb[,"CombValid"]<-ifelse(pmatch(FPcomb$`2RScomb`,RCpart4$FlightComb)>0,TRUE,FALSE)
+FPcomb2<-subset(FPcomb,FPcomb$CombValid==TRUE)
+
+# idea
+# use grep to generate matching int
+# use length >0 to get match pair
+# matching = ture for sorting
+
+
+
+
+
+FPcomb2<-subset(FPcomb,FPcomb$CombValid=="TRUE")
+
+RS_all<-cbind(RS1FP,RS2FP)
+RS_all[,"CompleteFP"]<-paste(RS1FP$FP1_item,RS2FP$FP2_item,sep="")
+
+
+#----
 RS1<-xmlApply(getNodeSet(query1,
                          "//Fare_MasterPricerTravelBoardSearchReply/
                          flightIndex[requestedSegmentRef=1]/
